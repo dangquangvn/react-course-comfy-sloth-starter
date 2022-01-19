1e4117e6dbba218e7a49695f0e1550a187477595
quangTD <tsquang398@gmail.com>
2022-01-19 17:02:50 +0700

    :sparkles: [25 ECOMMERCE SLOTH SHOP] 290 React Router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  AboutPage,
+  ErrorPage,
+  HomePage,
+  ProductsPage,
+  SingleProductPage,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path={"/"} exact>
+          <HomePage />
+        </Route>
+        <Route path={"/about"}>
+          <AboutPage />
+        </Route>
+        <Route path={"/products"} exact>
+          <ProductsPage />
+        </Route>
+        <Route path={"/products/:id"} exact>
+          <SingleProductPage />
+        </Route>
+        <Route path={"*"} exact>
+          <ErrorPage />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
