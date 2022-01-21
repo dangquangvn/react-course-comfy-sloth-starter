530afe05463317355af665521916464320b159b8
quangTD <tsquang398@gmail.com>
2022-01-21 15:48:53 +0700

    :sparkles: [25 ECOMMERCE SLOTH SHOP] implement error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,16 +1,33 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 const ErrorPage = () => {
-  return <h4>error page</h4>
-}
+  return (
+    <Wrapper className='page-100'>
+      {/* <Wrapper > */}
+      {/* <section className='error-container'> */}
+      <section>
+        <h1>404</h1>
+        <h3>Sorry, the page you tried cannot be found</h3>
+        <Link to={"/"} className='btn'>
+          back home
+        </Link>
+      </section>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.main`
   background: var(--clr-primary-10);
   display: flex;
+  /* flex-direction: column; */
   justify-content: center;
   align-items: center;
   text-align: center;
+  .error-container {
+    min-height: calc(100vh - 10rem);
+    padding: 5rem 0;
+  }
   h1 {
     font-size: 10rem;
   }
@@ -18,6 +35,6 @@ const Wrapper = styled.main`
     text-transform: none;
     margin-bottom: 2rem;
   }
-`
+`;
 
-export default ErrorPage
+export default ErrorPage;
