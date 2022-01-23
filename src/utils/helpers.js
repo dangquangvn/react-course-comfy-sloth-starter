1ec6145d9ad644257eb6162cd71474e1ecb6d4de
quangTD <tsquang398@gmail.com>
2022-01-23 18:35:11 +0700

    :sparkles: [25 ECOMMERCE SLOTH SHOP] feat 308. format price with usd and vnd

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,13 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+  //Intl = international
+  // return new Intl.NumberFormat("en-US", {
+  //   style: "currency",
+  //   currency: "USD",
+  // }).format(number/100); number(cent) -> number/100 (usd)
+  return new Intl.NumberFormat("vi-VN", {
+    style: "currency",
+    currency: "VND",
+  }).format(number);
+};
 
-export const getUniqueValues = () => {}
+export const getUniqueValues = () => {};
