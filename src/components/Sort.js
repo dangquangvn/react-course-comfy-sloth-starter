ec0c0e016999761cb72785d81694b5f6d46ad2fc
quangTD <tsquang398@gmail.com>
2022-01-26 14:17:49 +0700

    :sparkles: [25 ECOMMERCE SLOTH SHOP] feat implement sort products

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,10 +1,52 @@
-import React from 'react'
-import { useFilterContext } from '../context/filter_context'
-import { BsFillGridFill, BsList } from 'react-icons/bs'
-import styled from 'styled-components'
+import React, { useDebugValue } from "react";
+import { useFilterContext } from "../context/filter_context";
+import { BsFillGridFill, BsList } from "react-icons/bs";
+import styled from "styled-components";
 const Sort = () => {
-  return <h4>sort </h4>
-}
+  const {
+    filtered_products: filtered,
+    grid_view,
+    handleGridView,
+    handleListView,
+    sort,
+    updateSort,
+  } = useFilterContext();
+  return (
+    <Wrapper>
+      <div className='btn-container'>
+        <button
+          className={`${grid_view ? "active" : ""}`}
+          onClick={handleGridView}
+        >
+          <BsFillGridFill />
+        </button>
+        <button
+          className={`${!grid_view ? "active" : ""}`}
+          onClick={handleListView}
+        >
+          <BsList />
+        </button>
+      </div>
+      <p className='text'>{filtered.length} products found</p>
+      <hr />
+      <form action=''>
+        <label htmlFor='sort'>sort by</label>
+        <select
+          name='sort'
+          id='sort'
+          className='sort-input'
+          value={sort}
+          onChange={(e) => updateSort(e.target.value)}
+        >
+          <option value='price-lowest'>price ( lowest )</option>
+          <option value='price-highest'>price ( highest )</option>
+          <option value='name-a'>name (a-z)</option>
+          <option value='name-z'>name (z-a)</option>
+        </select>
+      </form>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -67,6 +109,6 @@ const Wrapper = styled.section`
     font-size: 1rem;
     text-transform: capitalize;
   }
-`
+`;
 
-export default Sort
+export default Sort;
