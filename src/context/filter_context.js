5db3b9b9fa5869cde16d8e08232e4a290e7b509c
quangTD <tsquang398@gmail.com>
2022-01-26 14:16:31 +0700

    :sparkles: [25 ECOMMERCE SLOTH SHOP] feat setup filter context & reducer

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useContext, useReducer } from 'react'
-import reducer from '../reducers/filter_reducer'
+import React, { useEffect, useContext, useReducer } from "react";
+import reducer from "../reducers/filter_reducer";
 import {
   LOAD_PRODUCTS,
   SET_GRIDVIEW,
@@ -9,21 +9,60 @@ import {
   UPDATE_FILTERS,
   FILTER_PRODUCTS,
   CLEAR_FILTERS,
-} from '../actions'
-import { useProductsContext } from './products_context'
+} from "../actions";
+import { useProductsContext } from "./products_context";
 
-const initialState = {}
+const initialState = {
+  filtered_products: [],
+  all_products: [],
+  grid_view: true,
+  sort: "price-lowest",
+};
 
-const FilterContext = React.createContext()
+const FilterContext = React.createContext();
 
 export const FilterProvider = ({ children }) => {
+  const { products } = useProductsContext();
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  console.log(
+    "🚀TCL: ~ file: filter_context.js ~ line 21 ~ filtered_products",
+    state.filtered_products
+  );
+  useEffect(() => {
+    dispatch({ type: LOAD_PRODUCTS, payload: { data: products } });
+  }, [products]);
+
+  const handleGridView = () => {
+    dispatch({ type: SET_GRIDVIEW });
+  };
+  const handleListView = () => {
+    dispatch({ type: SET_LISTVIEW });
+  };
+  const updateSort = (value) => {
+    // for demonstration:
+    // const name = e.target.name
+    // const value = e.target.value
+    dispatch({ type: UPDATE_SORT, payload: { sort: value } });
+  };
+  const handleSortProducts = () => {
+    console.log("handleSortProducts", state.sort);
+    // dispatch({ type: SORT_PRODUCTS, payload: { value: state.sort } });
+    dispatch({ type: SORT_PRODUCTS });
+  };
+  useEffect(() => {
+    handleSortProducts();
+  }, [state.sort]);
+
   return (
-    <FilterContext.Provider value='filter context'>
+    <FilterContext.Provider
+      value={{ ...state, handleGridView, handleListView, updateSort }}
+    >
       {children}
     </FilterContext.Provider>
-  )
-}
+  );
+};
 // make sure use
 export const useFilterContext = () => {
-  return useContext(FilterContext)
-}
+  return useContext(FilterContext);
+};
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -7,11 +7,86 @@ import {
   UPDATE_FILTERS,
   FILTER_PRODUCTS,
   CLEAR_FILTERS,
-} from '../actions'
+} from "../actions";
+import { sortType } from "../utils/constants";
 
-const filter_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+// const filter_reducer = (state, action) => {
+const filter_reducer = (state, { type, payload }) => {
+  switch (type) {
+    case LOAD_PRODUCTS:
+      return {
+        ...state,
+        // filtered_products: payload.data,
+        // all_products: payload.data,
+        filtered_products: [...payload.data],
+        all_products: [...payload.data],
+      };
+    case SET_GRIDVIEW:
+      return { ...state, grid_view: true };
+    case SET_LISTVIEW:
+      return { ...state, grid_view: false };
+    case UPDATE_SORT:
+      return { ...state, sort: payload.sort };
+    case SORT_PRODUCTS: {
+      // let tempSort = payload.value || sort.PRICE_LOWEST;
+      const { sort, filtered_products } = state;
+      console.log(
+        "🚀TCL: ~ file: filter_reducer.js ~ line 33 ~ sort",
+        sort,
+        sort.PRICE_HIGHEST
+      );
+      let tempProducts = [...filtered_products];
+      // if (tempSort === sort.PRICE_LOWEST) {
+      if (sort === sortType.PRICE_LOWEST) {
+        console.log("price-lowest === true");
+        tempProducts = state.filtered_products.sort(
+          (a, b) => a.price - b.price
+        );
+        // } else if (tempSort === sort.PRICE_HIGHEST) {
+      } else if (sort === sortType.PRICE_HIGHEST) {
+        console.log("price-highest === true");
+        //= short work
+        // tempProducts = state.filtered_products.sort(
+        //   (a, b) => b.price - a.price
+        // );
+        //= long work
+        tempProducts = state.filtered_products.sort((a, b) => {
+          // if A < B (or return negative)
+          // sort will put A before B
+          if (a.price < b.price) {
+            return -1;
+          }
+          // if A > B (or return positive)
+          // sort will put A after B
+          if (a.price > b.price) {
+            return 1;
+          }
+          return 0;
+        });
+        // } else if (tempSort === sort.NAME_A) {
+      } else if (sort === sortType.NAME_A) {
+        console.log("name_a === true");
+        tempProducts = state.filtered_products.sort(
+          (a, b) => "" + a.name.localeCompare(b.name)
+        );
+      } else {
+        console.log("name_z === true");
+        // tempProducts = state.filtered_products.sort((a, b) => b.name - a.name);
+        tempProducts = state.filtered_products.sort(
+          (a, b) => "" + b.name.localeCompare(a.name)
+        );
+      }
+      console.log(
+        "🚀TCL: ~ file: filter_reducer.js ~ line 51 ~ tempProducts",
+        tempProducts
+      );
+      return { ...state, filtered_products: tempProducts };
+      // return state;
+    }
+    default:
+      throw new Error(`No Matching "${type}" - action type`);
+  }
+  // return state
+};
 
-export default filter_reducer
+export default filter_reducer;
